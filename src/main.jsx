6a83c5a9fffe2961041574908dc8ae80a5acff6d
Bohdan Miskov <bohdanmiskov07@gmail.com>
2025-05-31 20:52:23 +0300

    Finish page

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import { PersistGate } from 'redux-persist/integration/react';
 import { App } from './App.jsx';
 
 import 'modern-normalize/modern-normalize.css';
 import './index.css';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { persistor, store } from './reduxState/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Provider store={store}>
+        <PersistGate loading={null} persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
 );
---
+++ src/reduxState/currency/operations.js
@@ -0,0 +1,36 @@
+import { createAsyncThunk } from '@reduxjs/toolkit';
+import { getUserInfo } from '../../service/opencagedataApi';
+import { exchangeCurrency } from '../../service/exchangeAPI';
+
+export const fetchBaseCurrency = createAsyncThunk(
+  'currency/fetchBaseCurrency',
+  async (coords, thunkApi) => {
+    const state = thunkApi.getState();
+    const { baseCurrency } = state.currency;
+
+    if (baseCurrency) {
+      return thunkApi.rejectWithValue(
+        'This user heve had base currency already/',
+      );
+    }
+
+    try {
+      const data = await getUserInfo(coords);
+      return data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message);
+    }
+  },
+);
+
+export const fetchExchangeInfo = createAsyncThunk(
+  'currency/fetchExchangeInfo',
+  async (cred, thunkApi) => {
+    try {
+      const data = await exchangeCurrency(cred);
+      return data;
+    } catch (error) {
+      return thunkApi.rejectWithValue(error.message);
+    }
+  },
+);
